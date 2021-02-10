be894ebbcc4e4de857d87cdd4423a7da23b36edc
bvbasto <bvbasto@outlook.com>
2021-02-10 02:17:08 +0000

    Delete notebooks/Users/bbasto@microsoft.com/play/univ/DLUDH directory

--- notebooks/Users/bbasto@microsoft.com/play/univ/DLUDH/25x.Exercicio2.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# MAGIC %md
-# MAGIC 
-# MAGIC o ficheiros /training/filesDub/data.csv tem uma lista dos ficheiros existentes num determinado diretorio
-# MAGIC o ficheiros /training/filesDub/gest.csv tem uma lista das repetiçõs desses ficheiros. Cada repetição passa a ter um sufixo que é indicado na própria tabela 
-# MAGIC 
-# MAGIC crie um novo ficheiro em que cada linha tem o nome do ficheiro principal e o numero de repetições que exitem em gest e se existem linhas no ficheiro de repetições que não têm referência no ficheiro data
